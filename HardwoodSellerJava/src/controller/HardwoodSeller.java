cb41beeeedc1db772ed475e46743056e1d37028e
Marc Tifrea <marc.tifrea@gmail.com>
2016-02-25 12:28:52 -0600

    Implemented input method
    Fleshed out the method for readInputFile, it will now read a file and store the WoodItems in an array with their cost and time

--- HardwoodSellerJava/src/controller/HardwoodSeller.java
+++ HardwoodSellerJava/src/controller/HardwoodSeller.java
@@ -1,14 +1,33 @@
+package controller;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.*;
 /**
  * 
  */
-package controller;
+
 
 /**
  * @author Esteban
  *
  */
 public class HardwoodSeller {
+	
+	private double cherryCost = 5.95;
+	private double curlyMapelCost = 6.00;
+	private double mahoganyCost = 9.60;
+	private double wengeCost = 22.35;
+	private double whiteOakCost = 6.70;
+	private double sawDustCost = 1.5;
 
+	private double cherryTime = 2.5;
+	private double curlyMapelTime = 1.5;
+	private double mahoganyTime = 3;
+	private double wengeTime = 5;
+	private double whiteOakTime = 2.3;
+	private double sawDustTime = 1;
+	
 	/**
 	 * @param args
 	 */
@@ -17,8 +36,40 @@ public class HardwoodSeller {
 
 	}
 	
-	public void readInputFile(String inputFilePath){
-		
+	public void readInputFile(String inputFilePath)
+	{
+		try
+		{
+			File f = new File(inputFilePath);
+			Scanner scan = new Scanner(f);
+			String []info = scan.nextLine().split(";");
+			String []woodInfo = scan.nextLine().split(";");
+			ArrayList<WoodItem> items = new ArrayList<WoodItem>();
+			for(String s : woodInfo)
+			{
+				WoodItem w;
+				String[] args = s.split(":");
+				String name = args[0];
+				if(name.equals("Cherry"))
+					w = new WoodItem(name, cherryTime, cherryCost);
+				else if(name.equals("Curly Mapel"))
+					w = new WoodItem(name, curlyMapelTime, curlyMapelCost);
+				else if(name.equals("Genuine Mahogany"))
+					w = new WoodItem(name, mahoganyTime, mahoganyCost);
+				else if (name.equals("Wenge"))
+					w = new WoodItem(name, wengeTime, wengeCost);
+				else if( name.equals("White Oak"))
+					w = new WoodItem(name, whiteOakTime, whiteOakCost);
+				else  
+					w = new WoodItem(name, sawDustTime, sawDustCost);
+				items.add(w);
+			}
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
 	}
 	
 	public Double deliveryTime(){
